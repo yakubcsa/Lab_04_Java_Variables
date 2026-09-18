bc77a63bdb5c9cc7cf9042c32ca88f08cb457c27
yakubcsa <yakubcsa@mail.uc.edu>
2026-09-18 13:22:57 -0400

    Add files via upload

---
+++ Lab_04_Java_Variables/src/Main.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    int intOperandA = 22;
+    int intOperandB = 44;
+    int intSum = 0;
+    int intProduct = 0;
+    int intDifference = 0;
+    int intQuotient = 0;
+    int intModulo = 0;
+
+    intSum = intOperandA + intOperandB; //sum of 22 and 44
+    IO.println("the sum of " + intOperandA + " + " + intOperandB + " is " + intSum);
+
+    intProduct = intOperandA * intOperandB;
+    IO.println("The product of " + intOperandA + " * " + intOperandB + " is " + intProduct);
+
+    intDifference = intOperandA - intOperandB;
+    IO.println("The product of " + intOperandA + " - " + intOperandB + " is " + intDifference);
+
+    intQuotient = intOperandA % intOperandB;
+    IO.println("The product of " + intOperandA + " % " + intOperandB + " is " + intQuotient);
+
+    double doubleOperandA = 15.50;
+    double doubleOperandB = 12.20;
+    double doubleSum = 0;
+    double doubleProduct = 0;
+    double doubleDifference = 0;
+    double doubleQuotient = 0;
+
+
+
+}
